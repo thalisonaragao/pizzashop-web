c25bdfa9e035f5824d93fcdd0e6758e23601b545
Thalison Aragão <thalissonaragao01@gmail.com>
2025-12-23 11:08:27 -0300

    Usando React Hook Form

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
-import { AppLayout } from './_layouts/app'
-import { AuthLayout } from './_layouts/auth'
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 
 export const router = createBrowserRouter([
     {
